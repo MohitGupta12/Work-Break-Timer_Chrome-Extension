647440276e867c43c61fd953155efa843961d448
MohitGupta12 <mohit2412gupta@gmail.com>
2022-04-13 19:11:42 +0530

    added alarm function that ring after every hour

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ const time_el = document.querySelector('.watch .timer');
 const start_btn = document.getElementById('start');
 const pause_btn = document.getElementById('pause');
 const reset_btn = document.getElementById('reset');
+const audio_el = document.getElementById('audio');
 
 let seconds = 0;
 let interval = null;
+let rep_completed = 1;
+let temp;
+audio_el.loop = true;
 
 //Event Listeners
 
@@ -18,30 +22,42 @@ reset_btn.addEventListener('click', reset)
 function timer() {
     
     seconds++;
-    
     let hrs = Math.floor(seconds / 3600);
     let mins = Math.floor((seconds - (hrs * 3600)) / 60);
     let secs = seconds % 60;
-
-    if (secs < 10) secs = '0' + secs;
-    if (mins < 10) mins = '0' + mins;
-    if (hrs < 10) hrs = '0' + hrs;
-
-    time_el.innerText = `${hrs}:${mins}:${secs}`;
     
+    if (secs < 10) secs = '0' + secs;
+    if (mins < 10) {
+        mins = '0' + mins;
+    }
+    if (hrs < 10) {
+        hrs = '0' + hrs;
+        temp = '0' + rep_completed;    
+    }
+    
+    time_el.innerText = `${hrs}:${mins}:${secs}`;
+
+    if (hrs == temp) {
+        RingAlarm();
+    }
+
+
 }
 
 function start(){
     if (interval) {
         return
     }
-
     interval = setInterval(timer, 1000);
+
 }
 
 function pause() {
     clearInterval(interval);
     interval = null;
+    audio_el.pause();
+    rep_completed++;
+    console.log(rep_completed)
 }
 
 function reset() {
@@ -49,6 +65,16 @@ function reset() {
     seconds = 0;
     time_el.innerText= `00:00:00`
 }
+
+function RingAlarm() {
+    console.log("Ringing...")
+    pause();
+    rep_completed--;
+    audio_el.play();
+}
+
+
+
 
 
 // Add function to beep a timer when hit 1 hour mark
